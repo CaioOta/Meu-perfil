b1bfd371b80dbcb1260deedd52a303d90fbdf1fe
CaioOta <caio.ryou@gmail.com>
2022-05-28 08:51:49 -0300

    feat: terminei 3 aula do html

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views/index.html'))
-)
-app.get('/sobre', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views/sobre.html'))
-)
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 app.listen(port, () => console.info(`O servidor ta rodando na porta ${port}`))
